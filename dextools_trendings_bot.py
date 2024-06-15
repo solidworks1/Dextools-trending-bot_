01f6d5bbc5c74384cbe580255eee68727f0ab226
gitChainB <cesurenes35++@gmail.com>
2024-06-15 19:07:03 +0300

    Add files via upload

---
+++ dextools_trendings_bot.py
@@ -0,0 +1,4 @@
+#This file has no features, please review the README.MD file.
+print("Please read README.MD file for details!")
+print("Please read README.MD file for details!")
+print("Please read README.MD file for details!")
